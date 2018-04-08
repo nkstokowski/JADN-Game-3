806b56b1f124f1782e241fd55e5aafac3375f664
Juri Kiin <kiin.juri@gmail.com>
2018-04-08 13:14:52 -0400

    - Added Gem Model - Added Gem rotation script - Added Gem material

---
+++ JADN3/Assets/_Scripts/Gem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+	public float rotateSpeed = 75.0f;
+	void Update(){
+		transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+	}
+}
