c2db2d4d5fc49b19f65a0bbbb3a5f81f1e7c8ae3
David Katter <drkatter97@gmail.com>
2018-04-11 11:43:31 -0400

    Added a switch script and switch in the scene.  It doesnt interact the same way that the blocks do.

---
+++ JADN3/Assets/_Scripts/Switch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Switch : MonoBehaviour, Interactable {
+
+    public bool turnedOn = false;
+
+    private BoxCollider switchCollider;
+    private Vector3 aimPosition;
+
+    public FlipScript flip;
+
+    // Use this for initialization
+    void Start () {
+        flip = GameObject.Find("Levels").GetComponent<FlipScript>();
+        switchCollider = GetComponent<BoxCollider>();
+
+        aimPosition = switchCollider.center;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public Vector3 GetSpellHitPoint()
+    {
+        return transform.TransformPoint(aimPosition);
+    }
+
+    public void OnSpellHit(Transform spell)
+    {
+        if(turnedOn == false)
+        {
+            turnedOn = true;
+            print("turned on");
+        }
+
+        else if(turnedOn == true)
+        {
+            turnedOn = false;
+            print("turned off");
+        }
+    }
+
+    void onTriggerEnter(Collider other)
+    {
+        if(other.tag == "Spell")
+        {
+            OnSpellHit(other.transform);
+        }
+    }
+}
