6d8d14e808a4284f78ee9c5cd8c99ecac7bf8266
akn7003 <akn7003@g.rit.edu>
2018-04-23 00:28:31 -0400

    Reload Scene Button

--- JADN3/Assets/_Scripts/Pause.cs
+++ JADN3/Assets/_Scripts/Pause.cs
@@ -9,11 +9,19 @@ public class Pause : MonoBehaviour {
 	bool isPaused = false;
 	public GameObject pausedText;
 	public GameObject menuText;
-	
+	public GameObject resetText;
+	public Scene scene;
+
 	// Update is called once per frame
 	void Update () {
+		scene = SceneManager.GetActiveScene ();
+
 		if(Input.GetKeyUp(KeyCode.Escape)){
 			TogglePaused();
+		}
+
+		if (Input.GetKeyUp (KeyCode.R)) {
+			SceneManager.LoadScene (scene.name);
 		}
 	}
 
@@ -22,12 +30,19 @@ public class Pause : MonoBehaviour {
 		if (isPaused){
 			menuText.SetActive(true);
 			pausedText.SetActive(true);
+			resetText.SetActive (true);
 			Time.timeScale = 0f;
 		} else {
 			menuText.SetActive(false);
 			pausedText.SetActive(false);
+			resetText.SetActive (false);
 			Time.timeScale = 1f;
 		}
+	}
+
+	public void Reload()
+	{
+		SceneManager.LoadScene (scene.name);
 	}
 
 	public void ReturnToMenu(){
