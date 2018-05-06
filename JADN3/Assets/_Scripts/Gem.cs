12fcec5500ab15247260b4ff8635067a14b069ec
Juri Kiin <kiin.juri@gmail.com>
2018-05-06 13:35:31 -0400

    - Added scene transitions

--- JADN3/Assets/_Scripts/Gem.cs
+++ JADN3/Assets/_Scripts/Gem.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Gem : MonoBehaviour {
 
 	public float rotateSpeed = 75.0f;
+	public string loadSceneName;
+	public GameObject manager;
 	void Update(){
 		transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
 	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player") {
+			manager.GetComponent<GameManager>().LoadLevel(loadSceneName);
+		}
+	}
+
 }
